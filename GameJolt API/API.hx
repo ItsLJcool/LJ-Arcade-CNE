5bf85623186478c068f326ecc9490cd881c599bb
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-07-17 04:02:30 -0700

    uh

--- GameJolt API/API.hx
+++ GameJolt API/API.hx
@@ -11,12 +11,18 @@ public static var GameJolt = {
 	username: null, token: null,
 };
 
-var ndllName = "GameJolt NDLL/gamejolt-api";
+var ndllName = "gamejolt-api";
+
+var asyncStack:Array<Array<Dynamic>> = []; // [[name, args, result]]
+
+var async_call = NdllUtil.getFunction(ndllName, "async_ndll_call", 2);
 public function gamejolt_init() {
+	
+    NdllUtil.getFunction(ndllName, "set_ndll_name", 1)(ndllName); // because we love Neo (he is being lazy)
 	NdllUtil.getFunction(ndllName, "gamejolt_init", 2)(Type,
 	"2af137395810fabb4391a26fede73ad39a9ca69084cf103589472e0c0eb77325090638a68431fcd353a67a4e28260da3");
 	
-	var registerThread = NdllUtil.getFunction("gamejolt-api", "registerThread", 0);
+	var registerThread = NdllUtil.getFunction(ndllName, "registerThread", 0);
 	registerThread();
 	#if ALLOW_MULTITHREADING
 		for (i in 0...Main.gameThreads.length) {
@@ -24,12 +30,27 @@ public function gamejolt_init() {
 		}
 	#end
 
+	NdllUtil.getFunction(ndllName, "set_async_stack", 1)(asyncStack);
+
 	login(GameJolt.username, GameJolt.token);
 }
 
 public function login(username:String, token:String) {
 	GameJolt.username = username;
 	GameJolt.token = token;
-	var loginResponce = NdllUtil.getFunction(ndllName, "gamejolt_login", 2)(GameJolt.username, GameJolt.token);
-	trace(loginResponce);
+	
+	Main.execAsync(async_call("gamejolt_login", [GameJolt.username, GameJolt.token]));
+}
+
+function update(elapsed) {
+	while(asyncStack.length > 0) {
+	    // or asyncStack.pop() if you dont really care about the order.
+	    // Tho we recommend using the shift, or else certain results might take longer to process.
+		var arr = asyncStack.shift();
+		var func = arr[0];
+	    var args = arr[1];
+		var result = arr[2];
+	    // process the result
+		trace(func, args, result);
+	}
 }
