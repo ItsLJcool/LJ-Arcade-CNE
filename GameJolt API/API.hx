8a00fa3eddbcc59a6901cdc0d0e768599ff000ed
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-07-17 03:48:15 -0700

    pog

--- GameJolt API/API.hx
+++ GameJolt API/API.hx
@@ -1,26 +1,35 @@
 //a
 /**
-    GameJolt API Ndll for CodenameEngine © 2024 by Neo & ItsLJcool is licensed under CC BY-NC-SA 4.0.
-    To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
+	GameJolt API Ndll for CodenameEngine © 2024 by Neo & ItsLJcool is licensed under CC BY-NC-SA 4.0.
+	To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
 **/
 
 import funkin.backend.utils.NdllUtil;
 import Type;
 
 public static var GameJolt = {
-    username: null, token: null,
+	username: null, token: null,
 };
 
 var ndllName = "GameJolt NDLL/gamejolt-api";
 public function gamejolt_init() {
-    NdllUtil.getFunction(ndllName, "gamejolt_init", 2)(Type,
-    "2af137395810fabb4391a26fede73ad39a9ca69084cf103589472e0c0eb77325090638a68431fcd353a67a4e28260da3");
-    
-    NdllUtil.getFunction(ndllName, "gamejolt_login", 2)(GameJolt.username, GameJolt.token);
-    
-    var result = NdllUtil.getFunction(ndllName, "openSession_0", 0)();
-    trace(result);
-    
-    var result = NdllUtil.getFunction(ndllName, "closeSession_0", 0)();
-    trace(result);
+	NdllUtil.getFunction(ndllName, "gamejolt_init", 2)(Type,
+	"2af137395810fabb4391a26fede73ad39a9ca69084cf103589472e0c0eb77325090638a68431fcd353a67a4e28260da3");
+	
+	var registerThread = NdllUtil.getFunction("gamejolt-api", "registerThread", 0);
+	registerThread();
+	#if ALLOW_MULTITHREADING
+		for (i in 0...Main.gameThreads.length) {
+			Main.execAsync(registerThread);
+		}
+	#end
+
+	login(GameJolt.username, GameJolt.token);
+}
+
+public function login(username:String, token:String) {
+	GameJolt.username = username;
+	GameJolt.token = token;
+	var loginResponce = NdllUtil.getFunction(ndllName, "gamejolt_login", 2)(GameJolt.username, GameJolt.token);
+	trace(loginResponce);
 }
